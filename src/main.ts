410a75b7bd0f2070a4f9c95ea0ea392838219d67
James <jakwuh@gmail.com>
2020-01-21 23:57:01 +0300

    Update wf

--- src/main.ts
+++ src/main.ts
@@ -48,7 +48,7 @@ async function run() {
       deployment_id: deployment.data.id,
       state: initialStatus,
       log_url: defaultUrl,
-      target_url: url
+      environment_url: url
     });
 
     core.setOutput("deployment_id", deployment.data.id.toString());
